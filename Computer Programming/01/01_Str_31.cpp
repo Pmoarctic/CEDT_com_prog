6c9bc5890e463bd8c05aa71225e72629cfaae2fd
Pomboon Pomboonmee <t4ng1336@gmail.com>
2024-08-07 10:50:42 +0700

    01_str_31

--- Computer Programming/01/01_Str_31.cpp
+++ Computer Programming/01/01_Str_31.cpp
@@ -9,11 +9,26 @@ long gcd(long a, long b)
 
 int main()
 {
-    double num,notrep,rep;
-    cin >> num >> notrep >> rep;
-
-    int n1 = ceil(log10(rep+1));
-    int n2 = ceil(log10(notrep+1));
+    string nums,notreps,reps;
+    cin >> nums >> notreps >> reps;
     
-    cout << n1 << ' ' << log10(rep+1);
+    long dnotrep = notreps.size();
+    long drep = reps.size();
+    //long dnotrep = floor(log10(notrep) + 1);
+    //long drep = floor(log10(notrep) + 1);
+    long notrep = stol(notreps);
+    long rep = stol(reps);
+    long num = stol(nums);
+
+    long total = notrep*pow(10,drep)+rep;
+
+    long up = total - notrep;
+    
+    long down = (pow(10,dnotrep+drep) - pow(10,dnotrep));
+    
+    up = num*down + up;
+
+    long tmp = gcd(up,down);
+
+    cout << up/tmp << " / " << down/tmp;
 }
